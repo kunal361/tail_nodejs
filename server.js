546b032e22dfee3bb7ba54b233c2a263fd0304d7
Kunal Sheth <kunal@Kunals-MacBook-Pro.local>
2015-06-30 19:26:58 +0530

    Updated server.js and added required node modules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var fs = require('fs');
 var url = require('url');
-var io = require('../node_modules/socket.io');
+var io = require('socket.io');
 
 var server = http.createServer(function(request, response){
   
